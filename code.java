676a702368d72fcf37bcdae5f0e7f43d52381ee7
“ganesh” <ganesh86758089@gmail.com>
2022-06-13 13:38:07 +0000

    4commit

--- code.java
+++ code.java
@@ -3,9 +3,9 @@ public class AsciiValue {
     public static void main(String[] args) {
 
         char ch = 'a';
-        int ascii = ch;
+        int ascii = cha;
         // You can also cast char to int
-        int castAscii = (inst) ch;
+        int castAscii = (ist) ch;
 
         System.out.println("The ASCII value of " + ch + " is: " + ascii);
         System.out.println("The ASCII values of " + ch + " is: " + castAscii);
